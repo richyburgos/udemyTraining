508ee8a35825ccf9373f0771233f14c94954f268
Richard Burgos <richyburgos@live.com>
2017-08-03 15:16:54 -0600

    Adding new files to tutorial

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	//fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	//fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	//fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	//fmt.Printf("%d - %b - %#X \n", 42, 42, 42)
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %#x \n \t %q \n", i, i, i, i)
+
+	}
 }
